13e0f0d30756334b68d903a6680f0acf0d883c1c
miriam262 <talmida.tichnut.miriam.karmarsky@byb.org.il>
2025-02-16 02:02:06 +0200

    update dataContext1

--- server/Migrations/ToDoDbContextModelSnapshot.cs
+++ server/Migrations/ToDoDbContextModelSnapshot.cs
@@ -76,7 +76,7 @@ namespace TodoApi.Migrations
 
                     b.Property<bool>("IsValid")
                         .HasColumnType("tinyint(1)")
-                        .HasColumnName("isValid");
+                        .HasColumnName("IsValid");
 
                     b.Property<int>("UserId")
                         .HasColumnType("int")
